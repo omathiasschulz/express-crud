57f8c48c9d2997c1fad11d44d68f282a3514d587
Mathias Schulz <mathias@schulz.net.br>
2024-11-16 19:59:45 -0300

    feat(migration): add migration to the project

---
+++ src/migrations/produtor.migration.ts
@@ -0,0 +1,32 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class ProdutorMigration implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+        create type enum_culturas as enum ('soja', 'milho', 'algodao', 'cafe', 'cana_acucar');
+        create table produtor (
+          id serial primary key,
+          cpf_cnpj varchar(14) not null unique,
+          nome varchar(100) not null,
+          cidade varchar(100) not null,
+          sigla_uf char(2) not null,
+          nome_fazenda varchar(100) not null,
+          total_ha_fazenda numeric(10, 2) not null,
+          total_agricultavel_ha_fazenda numeric(10, 2) not null,
+          total_vegetacao_ha_fazenda numeric(10, 2) not null,
+          culturas_fazenda enum_culturas[] not null,
+          created_at timestamp,
+          updated_at timestamp,
+          deleted boolean default false
+        );
+      `),
+      undefined;
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `drop table produtor; drop type enum_culturas;`,
+      undefined,
+    );
+  }
+}
