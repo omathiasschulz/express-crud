ef03bf26ceeb81627411c8c2838a3e31c6a122d8
Mathias Schulz <mathias@schulz.net.br>
2024-11-16 23:23:21 -0300

    feat(crud): add producer crud

--- src/modules/produtor/produtor.controller.ts
+++ src/modules/produtor/produtor.controller.ts
@@ -3,10 +3,17 @@ import { ProdutorService } from './produtor.service';
 import { CreateProdutorDTO } from './dtos/create-produtor.dto';
 import { plainToInstance } from 'class-transformer';
 import { validateOrReject } from 'class-validator';
+import { UpdateProdutorDTO } from './dtos/update-produtor.dto';
 
+/**
+ * Class ProdutorController
+ */
 class ProdutorController {
   private readonly produtorService = new ProdutorService();
 
+  /**
+   * Criação de um novo registro
+   */
   create = async ({ body }: Request, res: Response): Promise<void> => {
     try {
       const dto = plainToInstance(CreateProdutorDTO, body);
@@ -19,24 +26,43 @@ class ProdutorController {
     }
   };
 
-  async findAll(req: Request, res: Response): Promise<void> {
-    const { body, params } = req;
-    res.status(200).json({});
-  }
+  /**
+   * Consulta todos os registros
+   */
+  findAll = async ({}: Request, res: Response): Promise<void> => {
+    const produtores = await this.produtorService.findAll();
+    res.status(200).json(produtores);
+  };
 
-  async findOne(req: Request, res: Response): Promise<void> {
-    const { body, params } = req;
-    res.status(200).json({});
-  }
+  /**
+   * Consulta um registro pelo id
+   */
+  findOne = async ({ params }: Request, res: Response): Promise<void> => {
+    const produtor = await this.produtorService.findOne(params._id);
+    res.status(200).json(produtor);
+  };
 
-  async update(req: Request, res: Response): Promise<void> {
-    const { body, params } = req;
-    res.status(200).json({});
-  }
+  /**
+   * Atualiza um registro pelo id
+   */
+  update = async ({ body, params }: Request, res: Response): Promise<void> => {
+    try {
+      const dto = plainToInstance(UpdateProdutorDTO, body);
+      await validateOrReject(dto);
 
-  async remove(req: Request, res: Response): Promise<void> {
-    const { body, params } = req;
-    res.status(200).json({});
-  }
+      const produtor = await this.produtorService.update(params._id, dto);
+      res.status(201).json(produtor);
+    } catch (err: any) {
+      res.status(400).json({ error: err?.message ?? 'Falha', details: err });
+    }
+  };
+
+  /**
+   * Remove um registro pelo id
+   */
+  remove = async ({ params }: Request, res: Response): Promise<void> => {
+    await this.produtorService.remove(params._id);
+    res.sendStatus(200);
+  };
 }
 export default new ProdutorController();
--- src/modules/produtor/produtor.service.ts
+++ src/modules/produtor/produtor.service.ts
@@ -1,7 +1,11 @@
 import { AppDataSource } from '../../core/data-source';
 import { CreateProdutorDTO } from './dtos/create-produtor.dto';
+import { UpdateProdutorDTO } from './dtos/update-produtor.dto';
 import { Produtor } from './entities/produtor.entity';
 
+/**
+ * Class ProdutorService
+ */
 export class ProdutorService {
   private readonly produtor = AppDataSource.getRepository(Produtor);
 
@@ -9,19 +13,21 @@ export class ProdutorService {
     return await this.produtor.save(dto);
   }
 
-  findAll() {
-    return true;
+  async findAll(): Promise<Produtor[]> {
+    return await this.produtor.find({ where: { deleted: false } });
   }
 
-  findOne(id: number) {
-    return true;
+  async findOne(id: string): Promise<Produtor | null> {
+    return await this.produtor.findOne({ where: { id, deleted: false } });
   }
 
-  update(id: number, updatePostDto: any) {
-    return true;
+  async update(id: string, dto: UpdateProdutorDTO) {
+    await this.produtor.update({ id }, dto);
+
+    return await this.produtor.findOne({ where: { id, deleted: false } });
   }
 
-  remove(id: number) {
-    return true;
+  async remove(id: string): Promise<void> {
+    await this.produtor.update({ id }, { deleted: true });
   }
 }
